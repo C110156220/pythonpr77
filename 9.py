cd90d32fa94fbff3bf9929ebe9dd06814520cbf9
unknown <C110156220@nkust.edu.tw>
2022-03-27 20:47:18 +0800

    03.27

--- 9.py
+++ 9.py
@@ -1,7 +1,13 @@
 # 字根與子字串
 s1 = input("請輸入一個字串")
 s2 = input("請輸入另一個字串")
-if s1 in s2 :
-    print("YES，包含在裡面")
-else :
-    print("NO，不含在裡面")
+if len(s1) <= len(s2):
+    if s1 in s2 :
+        print("YES，包含在裡面")
+    else :
+        print("NO，不含在裡面")
+if len(s1) >= len(s2):
+    if s2 in s1 :
+        print("YES，包含在裡面")
+    else :
+        print("NO，不含在裡面")
